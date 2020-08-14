8df5e636171dbc93a042487895e3158079f083b9
Klyse <Klaus.Pruenster@gmail.com>
2020-08-14 16:39:10 +0200

    added job service

---
+++ JobService/Application/Jobs/Commands/AddJob/AddJobCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Application.Jobs.Commands.AddJob
+{
+	public class AddJobCommand : IRequest
+	{
+		public class Handler : IRequestHandler<AddJobCommand, Unit>
+		{
+			public Task<Unit> Handle(AddJobCommand request, CancellationToken cancellationToken)
+			{
+				return Unit.Task;
+			}
+		}
+	}
+}
---
+++ JobService/Web/Controllers/JobsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Jobs.Commands.AddJob;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JobService.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class JobsController : ControllerBase
+	{
+		private IMediator _mediator;
+
+		public JobsController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Unit>> Post()
+		{
+			return await _mediator.Send(new AddJobCommand());
+		}
+	}
+}
